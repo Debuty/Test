d3cf46f679b93567dc2b7c1b22f021bf41a6d37e
Debuty <Kareemsaleh84020@yahoo.com>
2024-11-21 18:22:56 +0200

    My commit

---
+++ New.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+let Test_Input = document.querySelector(".More_about_Me_Button");
+
+
+
+Test_Input.addEventListener("click" , function(){window.scrollTo(0,0)})
+
+console.log( window.screenY);
+
+
+ let Feature_1 = document.querySelector(".features_One");
+
+ let Feature_2 = document.querySelector(".features_Two");
+
+ let Feature_3 = document.querySelector(".features_Three");
+
+
+
+ 
+
+
